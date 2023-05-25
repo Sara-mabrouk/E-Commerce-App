8bb49f0539ea0a508cb5f1be71a332e0721e097b
Your Name <you@example.com>
2023-05-25 14:57:32 +0300

    ADD Header And Footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,51 @@
 import React from 'react'
-
+import { Form, Link } from "react-router-dom";
+import main from "../images/main-banner-1.jpg";
+import catbanner from "../images/catbanner-01.jpg";
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='home-wrapper-1 py-5'>
+        <div className='container-xxl'>
+        <div className="row">
+          <div className="col-6">
+            <div className="main-banner position-relative ">
+              <img
+                src={main}
+                className="img-fluid rounded-3"
+                alt="main banner"
+              />
+              <div className="main-banner-content position-absolute">
+                <h4>SUPERCHARGED FOR PROS.</h4>
+                <h5>iPad S13+ Pro.</h5>
+                <p>From $999.00 or $41.62/mo.</p>
+                <Link className="button">BUY NOW</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+              <div className="small-banner position-relative">
+                <img
+                  src={catbanner}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>Best Sake</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              </div>
+              </div>
+              </div>
+              </div>
+
+      </section>
+    </>
   )
 }
 
